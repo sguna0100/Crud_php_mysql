bd184199720e4bf89a20afd30eee9642f8eeafaf
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2021-08-19 08:20:57 +0530

    Update create_employee.php

--- create_employee.php
+++ create_employee.php
@@ -73,7 +73,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create Record</title>
+    <title>Employee - Create Record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
