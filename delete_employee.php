58d7d1f73f55f6802a74706439829961d6117f1e
Gunasekaran S <37099075+sguna0100@users.noreply.github.com>
2021-08-19 08:21:59 +0530

    Update delete_employee.php

--- delete_employee.php
+++ delete_employee.php
@@ -43,7 +43,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Delete Record</title>
+    <title>Employee - Delete Record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
